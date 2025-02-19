49f681594bf178a94740122efa4cfa4bf0471f6b
Familia <Familia@RodrAvilHP>
2025-02-18 20:24:18 -0500

    commit

---
+++ TallerProg1/src/co/edu/unbosque/view/HomePanel.java
@@ -0,0 +1,19 @@
+package co.edu.unbosque.view;
+
+import javax.swing.JPanel;
+
+public class HomePanel extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public HomePanel() {
+		setSize(1280, 720);
+		setLayout(null);
+		setVisible(true);
+
+	}
+
+}
